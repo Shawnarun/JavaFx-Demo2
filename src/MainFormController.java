7a89f99fc3b8025f66c14c720182d3da6b6d434a
ShawnArunjith <shawnarun09@gmail.com>
2024-02-20 21:53:32 +0530

    FEAT : Project initiated

--- src/MainFormController.java
+++ src/MainFormController.java
@@ -1,16 +1,29 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainFormController{
+    public ComboBox<String> cmbLanguages;
 
-
-    public TextField txtEmail;
-    public TextField txtResult;
+//    ArrayList<String> myLanguages = new ArrayList<>(
+//            Arrays.asList("Java","PHP","Kotlin","Ruby")
+//    );
 
     public void initialize() {
-        txtEmail.textProperty().addListener((Observable,oldValue,newValue) ->{
-            txtResult.setText(oldValue);
-        }
-    );
+        cmbLanguages.getItems().addAll("Java","PHP","Kotlin","Ruby");
+
+//        ObservableList<String> obList = FXCollections.observableList(myLanguages);
+//        cmbLanguages.setItems(obList);
+
+//        for (String name: myLanguages) {
+//            cmbLanguages.getItems().add(name);
+//        }
+
+//        cmbLanguages.getItems().add("Java");
 
     }
 
